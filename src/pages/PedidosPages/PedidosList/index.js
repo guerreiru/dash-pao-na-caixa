f1166ef757b7e3247be4fa6f70a7a813e5de8994
Fernando <f.luhis@gmail.com>
2021-12-08 17:02:27 -0300

    page pedidos

--- src/pages/PedidosPages/PedidosList/index.js
+++ src/pages/PedidosPages/PedidosList/index.js
@@ -1,178 +1,203 @@
+import { Grid, Button } from "@material-ui/core";
 import React from "react";
-import { Button, Grid, MenuItem, TextField } from "@material-ui/core";
 import Header from "../../../components/Header";
 import TableProdutos from "../../../components/TableProdutos";
 import { api } from "../../../services/api";
-import ObjVal from "../../../utils/Functions/ObjecValue";
-import { BakeryContext } from "../../../context/BakeryContext";
-import { Container, Content, TableContainer, TableHeader, ItemPedido } from "./styles";
-import TableResumo from "../../../components/TableResumo";
+import {
+  Container,
+  Content,
+  ContentHeader,
+  FormGroup,
+  ResultHeader,
+  ResidentTitle,
+  ResumoTitle,
+} from "./styles";
 
-const turnos = [
-  {
-    value: 0,
-    label: "Manhã",
-  },
-  {
-    value: 1,
-    label: "Tarde",
-  },
-];
+const turnos = ["Manhã", "Tarde"];
 
 const PedidosList = () => {
-  const { bakeryOptions } = React.useContext(BakeryContext);
-
-  const [bakery, setBakery] = React.useState("");
-
   const [date, setDate] = React.useState(setDefaultDate());
   const [condominiums, setCondominiums] = React.useState([]);
-  const [condominiumSelected, setCondominiumSelected] = React.useState("");
-  const [turno, setTurno] = React.useState(0);
+  const [condominiumOptions, setCondominiumOptions] = React.useState([]);
+  const [condominiumSelected, setCondominiumSelected] = React.useState({
+    id: "0",
+    name: "",
+  });
 
-  async function loadCondominiums(bakeryId) {
-    try {
-      const res = await api.get("condominiums");
-      const condominiumsArray = res.data.data;
-      const condominiumsByBakeryId = condominiumsArray.filter(
-        (item) => item.bakery.id === bakeryId
+  const [bakeries, setBakeries] = React.useState([]);
+  const [bakerySelected, setBakerySelected] = React.useState({
+    id: "0",
+    name: "",
+  });
+
+  const [turno, setTurno] = React.useState("");
+
+  React.useEffect(() => {
+    loadBakeries();
+  }, [bakeries]);
+
+  React.useEffect(() => {
+    loadCondominiums();
+  }, [condominiums]);
+
+  React.useEffect(() => {
+    async function getConByBakeryId() {
+      const conByBakeryId = condominiums.filter(
+        (cond) => cond.bakery.id === +bakerySelected.id
       );
-      setCondominiums(condominiumsByBakeryId);
-    } catch (error) {
-      console.error(error);
+      setCondominiumOptions(conByBakeryId);
     }
+    getConByBakeryId();
+  }, [bakerySelected, condominiums]);
+
+  async function loadBakeries() {
+    const res = await api.get(`bakeries`);
+    setBakeries(res.data.data);
   }
 
-  function handleSelectBakery({ target }) {
-    setBakery(target.value);
-    loadCondominiums(target.value);
+  async function loadCondominiums() {
+    const res = await api.get(`condominiums`);
+    setCondominiums(res.data.data);
   }
 
   function setDefaultDate() {
     return new Date().toISOString().slice(0, 10);
   }
 
+  function handleBakerySelected(ev) {
+    const bakeryName = bakeries.filter(
+      (bakery) => bakery.id === +ev.target.value
+    );
+    setBakerySelected({
+      id: ev.target.value,
+      name: bakeryName[0].name,
+    });
+
+    setCondominiumSelected({ id: 0, name: "" });
+  }
+
+  function handleCondominiumSelected(ev) {
+    const condominium = condominiums.filter(
+      (condominium) => condominium.id === +ev.target.value
+    );
+    setCondominiumSelected({
+      id: ev.target.value,
+      name: condominium[0].name,
+    });
+  }
+
   return (
     <Container>
       <Header loc="/dash" />
       <Content>
-        <TableContainer>
-          <TableHeader>
-            <h3>Pedidos</h3>
-          </TableHeader>
-
-          <Grid
-            container
-            rowSpacing={1}
-            columnSpacing={{ xs: 1, sm: 2, md: 3 }}
-          >
-            <Grid item xs={12} sm={6} md={3}>
-              <TextField
-                size="small"
-                InputLabelProps={{ shrink: true }}
-                name="purchase_datetime"
-                label="Data"
+        <ContentHeader>
+          <h3>Pedidos</h3>
+        </ContentHeader>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={12} sm={6} md={3}>
+            <FormGroup>
+              <label htmlFor="purchase_datetime">Data do pedido</label>
+              <input
+                id="purchase_datetime"
                 type="date"
                 value={date}
                 onChange={({ target }) => setDate(target.value)}
-                helperText="Selecione a data dos pedidos"
-                fullWidth
               />
-            </Grid>
-
-            {date.length !== 0 ? (
-              <Grid item xs={12} sm={6} md={3}>
-                <TextField
-                  size="small"
-                  id="bakery"
-                  select
-                  label="Padaria"
-                  value={bakery}
-                  onChange={handleSelectBakery}
-                  helperText="Selecione a padaria"
-                  fullWidth
-                >
-                  {ObjVal(bakeryOptions()).map((option) => (
-                    <MenuItem key={option.id} value={option.id}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </Grid>
-            ) : null}
-
-            {bakery.length !== 0 ? (
-              <Grid item xs={12} sm={6} md={3}>
-                <TextField
-                  size="small"
-                  id="condominiums"
-                  select
-                  label="Condominínio"
-                  value={condominiumSelected || ""}
-                  onChange={({ target }) =>
-                    setCondominiumSelected(target.value)
-                  }
-                  helperText="Selecione o condomínio"
-                  fullWidth
-                >
-                  {condominiums.map((option) => (
-                    <MenuItem key={option.id} value={option.id}>
-                      {option.name}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </Grid>
-            ) : null}
-
-            {condominiumSelected.length !== 0 ? (
-              <Grid item xs={12} sm={6} md={3}>
-                <TextField
-                  size="small"
-                  id="turno"
-                  select
-                  label="Turno"
-                  value={turno}
-                  onChange={({ target }) => setTurno(target.value)}
-                  helperText="Selecione o turno"
-                  fullWidth
-                >
-                  {turnos.map((option) => (
-                    <MenuItem key={option.value} value={option.value}>
-                      {option.label}
-                    </MenuItem>
-                  ))}
-                </TextField>
-              </Grid>
-            ) : null}
-
-            {bakery.length !== 0 && condominiumSelected.length !== 0 ? (
-              <Grid item xs={12} sx={{ marginBottom: "16px" }}>
-                <Button type="button" variant="contained">
-                  Filtrar
-                </Button>
-              </Grid>
-            ) : null}
+            </FormGroup>
           </Grid>
 
+          <Grid item xs={12} sm={6} md={3}>
+            <FormGroup>
+              <label htmlFor="padaria">Padaria</label>
+              <select
+                id="padaria"
+                value={bakerySelected.id}
+                onChange={handleBakerySelected}
+              >
+                <option value="0" disabled>
+                  Selecione a Padaria
+                </option>
+                {bakeries.map((bakery) => (
+                  <option key={bakery.id} value={bakery.id}>
+                    {bakery.name}
+                  </option>
+                ))}
+              </select>
+            </FormGroup>
+          </Grid>
+
+          {bakerySelected.id > 0 ? (
+            <Grid item xs={12} sm={6} md={3}>
+              <FormGroup>
+                <label htmlFor="condominio">Condomínio</label>
+                <select
+                  id="condominio"
+                  value={condominiumSelected.id}
+                  onChange={handleCondominiumSelected}
+                >
+                  <option value="0" disabled>
+                    Selecione o Condomínio
+                  </option>
+                  {condominiumOptions.map((condominium) => (
+                    <option key={condominium.id} value={condominium.id}>
+                      {condominium.name}
+                    </option>
+                  ))}
+                </select>
+              </FormGroup>
+            </Grid>
+          ) : null}
+
+          {condominiumSelected.id > 0 ? (
+            <Grid item xs={12} sm={6} md={3}>
+              <FormGroup>
+                <label htmlFor="turno">Turno</label>
+                <select
+                  id="turno"
+                  value={turno}
+                  onChange={({ target }) => setTurno(target.value)}
+                >
+                  <option value="" disabled>
+                    Turno
+                  </option>
+                  {turnos.map((turno) => (
+                    <option key={turno} label={turno}>
+                      {turno}
+                    </option>
+                  ))}
+                </select>
+              </FormGroup>
+            </Grid>
+          ) : null}
+
+          <Grid item xs={12}>
+            <Button disabled={turno === ""} type="button" variant="contained">
+              Filtrar
+            </Button>
+          </Grid>
+        </Grid>
+
+        <ResultHeader>
           <h3>
-            Padaria: Padaria Santo Antônio - Condominínio: Condominínio FAMA
+            {bakerySelected.name} {`- ${condominiumSelected.name} `}{" "}
+            {`- ${turno}`}
           </h3>
+        </ResultHeader>
 
-          <ItemPedido>
-            <h4>Residente: Jośe Silva - Apartamento: 12</h4>
-            <h5>Itens do pedido</h5>
-            <TableProdutos />
-          </ItemPedido>
+        <ResidentTitle>
+          <h4>Residente: Fernando - Apto. 12</h4>
+        </ResidentTitle>
+        <TableProdutos />
 
-          <ItemPedido>
-            <h4>Residente: Roberta - Apartamento: 20</h4>
-            <h5>Itens do pedido</h5>
-            <TableProdutos />
-          </ItemPedido>
+        <ResidentTitle>
+          <h4>Residente: Luiz - Apto. 15</h4>
+        </ResidentTitle>
+        <TableProdutos />
 
-          <h4 style={{marginTop: "16px"}} >Resumo</h4>
-          <TableResumo />
-
-        </TableContainer>
+        <ResumoTitle>
+          <h2>Resumo</h2>
+        </ResumoTitle>
+        <TableProdutos />
       </Content>
     </Container>
   );
--- src/pages/PedidosPages/PedidosList/styles.js
+++ src/pages/PedidosPages/PedidosList/styles.js
@@ -3,50 +3,21 @@ import styled from "styled-components";
 export const Container = styled.div`
   background-color: var(--bg-white);
   height: 100%;
-
-  .btnAddMobile {
-    display: none;
-  }
-
-  @media (max-width: 568px) {
-    .btnAddDesktop {
-      display: none;
-    }
-
-    .btnAddMobile {
-      display: inline-block;
-
-      &:hover {
-        color: #737373;
-        transition: 0.2s ease-in;
-      }
-    }
-  }
 `;
 
 export const Content = styled.div`
-  margin: 0 20px;
-`;
-
-export const TableContainer = styled.div`
+  margin: 20px;
   padding: 20px 30px;
   @media (max-width: 568px) {
     padding: 20px;
   }
 
   background-color: white;
-  margin: 20px 0;
   border-radius: 4px;
   box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.3);
-
-  a {
-    text-transform: capitalize;
-    text-decoration: none;
-    color: #737373;
-  }
 `;
 
-export const TableHeader = styled.div`
+export const ContentHeader = styled.div`
   margin-bottom: 20px;
   display: flex;
   flex-wrap: wrap;
@@ -69,13 +40,32 @@ export const TableHeader = styled.div`
   }
 `;
 
-export const ItemPedido = styled.div`
-  h4 {
-    margin: 8px 0;
-  }
+export const ResultHeader = styled.div`
+  margin-top: 8px;
+`;
 
-  h5 {
-    text-align: center;
+export const ResidentTitle = styled.div`
+  margin: 8px 0;
+`;
+
+export const ResumoTitle = styled.div`
+  margin: 8px 0;
+`;
+
+export const FormGroup = styled.div`
+  margin-bottom: 8px;
+  label {
+    display: block;
     margin-bottom: 8px;
   }
-`
+
+  input,
+  select {
+    height: 56px;
+    padding: 0 8px;
+    font-size: 16px;
+    border-radius: 4px;
+    border: 1px solid rgba(0, 0, 0, 0.5);
+    width: 100%;
+  }
+`;
